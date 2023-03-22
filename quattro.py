7e9b4c509b7564aedea0f497b9bcf68d12323752
Lich-senpai13 <papesenki.enrico@iisgalvanimi.edu.it>
2023-03-22 13:52:51 +0000

    commit4

--- quattro.py
+++ quattro.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request
-import math 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def calcoloGeo():
-  return render_template("tre_3.html", methods = ['GET']) 
+  return render_template("quattro_4.html", methods = ['GET']) 
 
 @app.route('/risultato',methods=['get'])
 def risultato():
@@ -15,11 +14,10 @@ def risultato():
         if tipo == 0:
              calcolo = a * a
         elif tipo == 1:
-            calcolo = math.sqrt(2)*a
-            
+            calcolo = (a**2) + (a**2)
         return calcolo
 
-    return render_template('tre_3Risultato.html',calcolo=doppietta(latoQ))
+    return render_template('quattro_4.Risoluzione.html',calcolo=doppietta(latoQ))
     
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
